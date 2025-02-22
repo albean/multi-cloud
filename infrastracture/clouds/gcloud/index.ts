0c62268ee9f3ca1405be991712f95a3f6ecb19c6
Al Bean <43551169+albean@users.noreply.github.com>
2025-02-22 18:05:38 +0100

    Commented out Docker build steps, hardcoded IMAGE variable in GCloud pipeline 🚧💡

--- infrastracture/clouds/gcloud/index.ts
+++ infrastracture/clouds/gcloud/index.ts
@@ -149,26 +149,28 @@ const PipelineImplementation = implement(Pipeline, (p): {  } => {
     },
     buildAttribute: {
       step: [
-        {
-          name: "gcr.io/cloud-builders/docker",
-          script: [
-            "docker build --platform linux/amd64 --progress plain -t backend -f backend/Dockerfile .",
-            "echo 'Building...'",
-            `export TAG="$(date +%y%m%d)-$(openssl rand -hex 16 | head -c 10)"`,
-            "echo $REPO:$TAG",
-            "docker tag backend $REPO:$TAG",
-            "docker push $REPO:$TAG",
-            "echo $REPO:$TAG > image.txt",
-          ].join(";\n"),
-          env: [
-            `REPO=${image}`,
-          ]
-        },
+        // {
+        //   name: "gcr.io/cloud-builders/docker",
+        //   script: [
+        //     "docker build --platform linux/amd64 --progress plain -t backend -f backend/Dockerfile .",
+        //     "echo 'Building...'",
+        //     `export TAG="$(date +%y%m%d)-$(openssl rand -hex 16 | head -c 10)"`,
+        //     "echo $REPO:$TAG",
+        //     "docker tag backend $REPO:$TAG",
+        //     "docker push $REPO:$TAG",
+        //     "echo $REPO:$TAG > image.txt",
+        //   ].join(";\n"),
+        //   env: [
+        //     `REPO=${image}`,
+        //   ]
+        // },
         {
           name: "gcr.io/google.com/cloudsdktool/cloud-sdk",
           script: [
             `ls -la`,
-            `export IMAGE=$(cat image.txt)`,
+            // @FIXME image
+            // `export IMAGE=$(cat image.txt)`,
+            `export IMAGE="europe-central2-docker.pkg.dev/ultimate-life-396919/backend/main:250222-71ae64ecef"`,
             `echo "Deploying location $IMAGE"`,
             p.services.map(_ => $gcloud(_)).map(s =>
               `gcloud run deploy ${s.tfService.name} --image $IMAGE --region ${location}`,
