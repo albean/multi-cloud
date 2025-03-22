19c487171664c96da9e7580dac072472a4808950
Al Bean <43551169+albean@users.noreply.github.com>
2025-03-22 19:10:20 +0100

    Removal of PDF rendering functionality and debug commands 🗑️

--- backend/commands/server.ts
+++ backend/commands/server.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from 'express';
 import { events, db, OrderFields } from "backend/schema"
 import cors from 'cors';
 import { eq } from 'drizzle-orm';
-import { renderPdf } from 'backend/services/pdf/render';
-import { sendmail } from './sendmail';
 import { ctx } from 'backend/Context';
 import { getEnv } from 'common/utils';
 
@@ -13,10 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static('public'))
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World v1240' });
-});
-
 app.get('/events', async (req: Request, res: Response) => {
   const eventsRes = await db.select().from(events)
 
@@ -25,7 +19,6 @@ app.get('/events', async (req: Request, res: Response) => {
 
 app.post('/buy', async (req: Request, res: Response) => {
   const data = req.body;
-
 
   const event = (await db.select().from(events).where(eq(events.id, data.id)))[0];
 
@@ -45,13 +38,6 @@ app.post('/buy', async (req: Request, res: Response) => {
 const port = getEnv("PORT", "8080");
 
 export const server = async () => {
-
-  // await ctx.mailQueue.send({
-  //   mail: "test@wp.pl",
-  //   fullName: `lol olo`,
-  //   eventId: 3,
-  // })
-
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
--- frontend/utils/react/useAsync.ts
+++ frontend/utils/react/useAsync.ts
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from 'react';
 
 export function useAsync<T>(asyncFn: () => Promise<T>) {
